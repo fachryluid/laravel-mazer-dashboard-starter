1ed169b2818f5eb5bb4382ca46c0ed9409fea8b9
fachiri <fachrycooles@gmail.com>
2024-09-26 14:42:13 +0800

    style: remove redundant semicolon

--- database/migrations/2024_02_26_174440_create_admins_table.php
+++ database/migrations/2024_02_26_174440_create_admins_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');;
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
