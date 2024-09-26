3d5987ca3e347570a5d44fcbfc635e1ed052a87a
fachiri <fachrycooles@gmail.com>
2024-09-26 09:32:29 +0800

    feat(constants): add all method to each class constant

--- app/Constants/UserGender.php
+++ app/Constants/UserGender.php
@@ -2,10 +2,20 @@
 
 namespace App\Constants;
 
+use ReflectionClass;
+use Illuminate\Support\Collection;
+
 class UserGender
 {
+  // NOTE:
+  // There are only 2 genders
+
   public const MALE = 'Laki-laki';
   public const FEMALE = 'Perempuan';
-  
-  // TIDAK LEBIH TIDAK KURANG!
+
+  public static function all(): Collection
+  {
+    $class = new ReflectionClass(__CLASS__);
+    return collect($class->getConstants());
+  }
 }
--- app/Constants/UserRole.php
+++ app/Constants/UserRole.php
@@ -2,9 +2,18 @@
 
 namespace App\Constants;
 
+use ReflectionClass;
+use Illuminate\Support\Collection;
+
 class UserRole
 {
   public const USER = 'USER';
   public const ADMIN = 'ADMIN';
   public const MANAGER = 'MANAGER';
+
+  public static function all(): Collection
+  {
+    $class = new ReflectionClass(__CLASS__);
+    return collect($class->getConstants());
+  }
 }
