f67b0c79dec6bf75cb59d41a231429a4479658be
fachiri <fachrycooles@gmail.com>
2024-09-26 09:35:28 +0800

    refactor(alerts): improve error alert handling for single and multiple errors

--- resources/views/components/main/alerts.blade.php
+++ resources/views/components/main/alerts.blade.php
@@ -7,11 +7,15 @@
 
 @if ($errors->any())
 	<div class="alert alert-light-danger color-danger alert-dismissible fade show">
-		<ul class="mb-0">
-			@foreach ($errors->all() as $error)
-				<li>{{ $error }}</li>
-			@endforeach
-		</ul>
+		@if (count($errors->all()) > 1)
+			<ul class="mb-0">
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		@else
+			<p>{{ $errors->all()[0] }}</p>
+		@endif
 		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 	</div>
 @endif
